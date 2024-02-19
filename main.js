9e11f09b487c9981ef0c9ad4d27c97c350fd6776
Janko Prester <janko.prester@corsmed.com>
2024-02-19 10:38:52 +0100

    add some comments

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ import { initScene, animate } from "./src/app.js";
 
 const assetManager = new AssetManager();
 
-document.getElementById("loading").style.display = "block";
+document.getElementById("loading").style.display = "block"; // Show loading screen
 
 try {
-  const assets = await assetManager.loadAssets();
+  const assets = await assetManager.loadAssets(); // Load assets
 
-  document.getElementById("loading").style.display = "none";
-  initScene(assets);
+  document.getElementById("loading").style.display = "none"; // Hide loading screen
+
+  initScene(assets); // Initialize scene and the app
   animate();
 } catch (error) {
   console.error("Error loading assets:", error);
